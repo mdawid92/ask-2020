c8af758ecfc52a8e051e31097e328e44743a8a1a
mdawid92 <michal.dawid92@gmail.com>
2020-05-01 12:37:48 +0200

    subvolumes and snapshots scripts

---
+++ snapshots.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+set -x
+
+sudo btrfs subvolume create /data/subvol
+sudo btrfs subvolume create /data/backups
+
+# create file
+sudo touch /data/subvol/test_file_1
+
+# create read-only backup snapshot
+sudo btrfs subvolume snapshot -r /data/subvol /data/backups/subvol
+
+sudo btrfs subvolume list /data/ 
+
+# check content
+ls -al /data/subvol
+ls -al /data/backups/subvol
+
+# delte test file
+sudo rm /data/subvol/test_file_1
+
+ls -al /data/subvol
+ls -al /data/backups/subvol
+
+# restore backup
+sudo btrfs subvolume delete /data/subvol
+sudo btrfs subvolume snapshot /data/backups/subvol /data/subvol
+
+# check after restore
+ls -al /data/subvol
+ls -al /data/backups/subvol
+
+
+# clean up after script
+sudo btrfs subvolume delete /data/subvol
+sudo btrfs subvolume delete /data/backups/subvol/
+sudo btrfs subvolume delete /data/backups
---
+++ subvolume.sh
@@ -0,0 +1,5 @@
+sudo btrfs subvolume create /data/sub1
+sudo btrfs subvolume create /data/sub2
+sudo btrfs subvolume create /data/sub2/sub2_sub
+
+sudo btrfs subvolume list /data/
